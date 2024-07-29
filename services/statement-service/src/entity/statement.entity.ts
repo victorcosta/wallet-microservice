e3ca7e922c2b64bf250f8d9dfa06e8fa7ce1f1d6
Joao Victor Dias <victorcostadev@gmail.com.br>
2024-07-29 20:04:25 -0300

    improve swagger in statement-service

--- services/statement-service/src/entity/statement.entity.ts
+++ services/statement-service/src/entity/statement.entity.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 
 export enum TransactionTypeRole {
@@ -11,26 +12,55 @@ export enum TransactionTypeRole {
 @Entity()
 export class Statement {
   @PrimaryGeneratedColumn()
+  @ApiProperty({
+    description: 'The ID of the Transaction',
+    example: '1',
+  })
   id: number;
 
   @Column()
+  @ApiProperty({
+    description: 'The ID of the user',
+    example: '2',
+  })
   userID: string;
 
   @Column()
+  @ApiProperty({
+    description: 'Description of the transaction',
+    example: 'Test transaction',
+  })
   description: string;
 
   @Column('decimal')
+  @ApiProperty({
+    description: 'Amount of the transaction',
+    example: 100,
+  })
   amount: number;
 
   @Column()
+  @ApiProperty({
+    description: 'Date of the transaction',
+    example: '2024-01-01T18:48:28.217Z',
+  })
   date: Date;
 
   @Column({
     type: 'enum',
     enum: TransactionTypeRole,
   })
+  @ApiProperty({
+    description: 'Type of the transaction',
+    enum: TransactionTypeRole,
+    example: TransactionTypeRole.ADDITION,
+  })
   type: TransactionTypeRole;
 
   @Column('decimal')
+  @ApiProperty({
+    description: 'Balance of the transaction',
+    example: 100,
+  })
   balance: number;
 }
